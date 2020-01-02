c05a8d01303ff7c476374fe579f6f782c425228c
matanana <MrBahur25@gmail.com>
2020-01-02 13:55:08 +0200

    printed the received message

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -7,9 +7,13 @@ import java.net.*;
 
 public class Server {
     private Utils utils;
+    private String teamName;
+    private DatagramSocket serverSocket;
 
-    public Server() {
+    public Server() throws SocketException {
         utils = new Utils();
+        teamName = "MatanAndOmer                    ";
+        serverSocket = new DatagramSocket(3117);
     }
 
     public static void main(String[] args) throws IOException {
@@ -19,14 +23,29 @@ public class Server {
     }
 
     private void listen() throws IOException {
-        DatagramSocket serverSocket = new DatagramSocket(3117);
         byte[] receiveData = new byte[1024];
         while (true) {
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
             String sentence = new String(receivePacket.getData());
-            System.out.println(sentence);
+            System.out.println("message received: " + sentence);
+            //move to different thread
+            Message recieved = new Message(sentence);
+
+            InetAddress address = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            sendOffer(address,port,recieved);
+
         }
+    }
+
+    private void sendOffer(InetAddress address, int port, Message received) throws IOException {
+        Message toSend = new Message(teamName, Type.OFFER, received.getHashToCrack(), received.getLength(), received.getStartRange(), received.getEndRange());
+        byte[] buffer = toSend.toString().getBytes();
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, port);
+        serverSocket.send(packet);
+        System.out.println("message sent: " + toSend.toString());
+
     }
 
     private String hash(String toHash) {
